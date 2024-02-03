21261b1445b0e5664990bbeb7380059a15f4342c
endogen <081081@gmx.net>
2024-02-03 02:45:37 +0100

    Added balance command

---
+++ plg/balance/balance.py
@@ -0,0 +1,31 @@
+import utils as utl
+import constants as con
+
+from plugin import TGBFPlugin
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler
+
+
+class Balance(TGBFPlugin):
+
+    async def init(self):
+        await self.add_handler(CommandHandler(self.handle, self.balance_callback, block=False))
+
+    @TGBFPlugin.send_typing
+    async def balance_callback(self, update: Update, context: CallbackContext):
+        wallet = await self.get_wallet(update.effective_user.id)
+        xian = await self.get_xian(wallet)
+
+        message = await update.message.reply_text(f"{con.WAIT} Retrieving balance ...")
+
+        try:
+            # Get balance
+            balance = xian.get_balance()
+        except Exception as e:
+            await message.edit_text(f"{con.ERROR} {e}")
+            msg = f"GET_BALANCE Error: {e}"
+            self.log.error(msg)
+            await self.notify(msg)
+            return
+
+        await message.edit_text(f"<code>XIAN: {balance}</code>")
