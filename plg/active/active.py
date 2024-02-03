af329f575d95184990f58f971b6768d54fc3f469
endogen <081081@gmx.net>
2024-02-03 02:43:23 +0100

    Adjusted formating

--- plg/active/active.py
+++ plg/active/active.py
@@ -10,13 +10,7 @@ class Active(TGBFPlugin):
             sql = await self.get_resource('create_active.sql')
             await self.exec_sql(sql)
 
-        await self.add_handler(
-            MessageHandler(
-                filters.ALL,
-                self.init_callback,
-                block=False
-            )
-        )
+        await self.add_handler(MessageHandler(filters.ALL, self.init_callback, block=False))
 
         self.run_repeating(self.cleaner_callback, 86_400)
 
