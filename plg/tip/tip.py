676847886a9f70f94eea06f679058c9f15e7ffad
Endogen <Endogen@users.noreply.github.com>
2024-10-20 02:11:43 +0200

    Adjusted 'tip' command to be able to use tokens

--- plg/tip/tip.py
+++ plg/tip/tip.py
@@ -20,8 +20,10 @@ class Tip(TGBFPlugin):
         if not update.message:
             return
 
-        if len(context.args) < 1:
-            await update.message.reply_text(await self.get_info())
+        if len(context.args) not in (1, 2):
+            await update.message.reply_text(
+                await self.get_info()
+            )
             return
 
         reply = update.message.reply_to_message
@@ -31,7 +33,49 @@ class Tip(TGBFPlugin):
             await update.message.reply_text(msg)
             return
 
-        amount = context.args[0]
+        contract = None
+        ticker = None
+        amount = None
+
+        to_user_id = reply.from_user.id
+        from_user_id = update.effective_user.id
+
+        message = await update.message.reply_text(f"{con.WAIT} Preparing ...")
+
+        # Tipping XIAN
+        if len(context.args) == 1:
+            contract = 'currency'
+            ticker = 'XIAN'
+            amount = context.args[0]
+
+        # Tipping token
+        elif len(context.args) == 2:
+            tokens = await self.get_plugin('tokens').get_tokens(from_user_id)
+
+            # It is a contract
+            if context.args[0].lower().startswith(('con_', 'currency')):
+                for token in tokens:
+                    if token[1] == context.args[0].lower():
+                        contract = token[1]
+                        ticker = token[2]
+                        break
+
+            # It is a ticker
+            else:
+                for token in tokens:
+                    if token[2] == context.args[0].upper():
+                        contract = token[1]
+                        ticker = token[2]
+                        break
+
+            amount = context.args[1]
+
+        if not contract:
+            await message.edit_text(
+                f'{con.ERROR} Unknown contract. Make sure you added this token to '
+                f'your token list first with <code>/token add contract_name</code>'
+            )
+            return
 
         try:
             # Check if amount is valid
@@ -47,26 +91,19 @@ class Tip(TGBFPlugin):
         if amount.is_integer():
             amount = int(amount)
 
-        to_user_id = reply.from_user.id
-        from_user_id = update.effective_user.id
-
         from_wallet = await self.get_wallet(from_user_id)
         xian = await self.get_xian(from_wallet)
-
-        usr_msg = str()
-        if len(context.args) > 1:
-            usr_msg = f"Message: {' '.join(context.args[1:])}"
 
         # Get address to which we want to tip
         to_address = (await self.get_wallet(to_user_id)).public_key
 
-        message = await update.message.reply_text(f"{con.WAIT} Sending...")
+        await update.message.edit_text(f"{con.WAIT} Sending...")
 
         try:
             # Send token
-            send = xian.send(amount, to_address)
+            send = xian.send(amount, to_address, token=contract)
         except Exception as e:
-            msg = f"SEND Error: {e}"
+            msg = f"TIP Error: {e}"
             self.log.error(msg)
             await self.notify(msg)
             await message.edit_text(f"{con.ERROR} {e}")
@@ -89,7 +126,7 @@ class Tip(TGBFPlugin):
                     from_user = update.effective_user.first_name
 
                 await message.edit_text(
-                    f"{con.MONEY} {html.escape(to_user)} received <code>{amount}</code> XIAN\n{link}",
+                    f"{con.MONEY} {html.escape(to_user)} received <code>{amount}</code> {ticker}\n{link}",
                     disable_web_page_preview=True
                 )
 
@@ -97,10 +134,10 @@ class Tip(TGBFPlugin):
                     # Notify user about tip
                     await context.bot.send_message(
                         to_user_id,
-                        f"You received <code>{amount}</code> XIAN from {from_user}\n{link}\n\n{usr_msg}",
+                        f"You received <code>{amount}</code> {ticker} from {from_user}\n{link}",
                         disable_web_page_preview=True
                     )
-                    self.log.info(f"User ID {to_user_id} notified about tip of {amount} XIAN")
+                    self.log.info(f"User ID {to_user_id} notified about tip of {amount} {ticker}")
                 except Exception as ex:
                     self.log.warning(f"User ID {to_user_id} could not be notified about tip: {ex} - {update}")
 
