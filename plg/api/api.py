0c5e45026d55f199e8302c0c967e5b22caef5b23
Endogen <Endogen@users.noreply.github.com>
2024-11-29 12:30:12 +0100

    Correctly handle invalid IDs

--- plg/api/api.py
+++ plg/api/api.py
@@ -7,9 +7,19 @@ class Api(TGBFPlugin):
     async def init(self):
         await self.add_endpoint('/address/{address_id}', self.address)
 
-    async def address(self, address_id: str):
+    async def address(self, telegram_id: str):
+        try:
+            int(telegram_id)
+        except ValueError:
+            response = {
+                'error': 'Invalid Telegram ID',
+                'success': False,
+                'public_key': ''
+            }
+            return JSONResponse(content=response)
+
         sql = await self.get_resource('select_address.sql')
-        data = await self.exec_sql_global(sql, int(address_id))
+        data = await self.exec_sql_global(sql, int(telegram_id))
 
         address = data['data']
 
