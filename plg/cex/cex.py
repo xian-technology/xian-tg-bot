b7c9e182bdd3b4b7f309ab8fd01c5f2bcf440c8b
Endogen <Endogen@users.noreply.github.com>
2025-02-04 00:39:20 +0100

    Add new /cex command

---
+++ plg/cex/cex.py
@@ -0,0 +1,19 @@
+from plugin import TGBFPlugin
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler
+
+
+class Cex(TGBFPlugin):
+
+    async def init(self):
+        await self.add_handler(CommandHandler(self.handle, self.cex_callback, block=False))
+
+    @TGBFPlugin.send_typing()
+    async def cex_callback(self, update: Update, context: CallbackContext):
+        # Don't deal with edited messages
+        if not update.message:
+            return
+
+        await update.message.reply_text(
+            await self.get_info()
+        )
