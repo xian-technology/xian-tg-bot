9cfeeac6b8b983a5c50f9ef61617139080d22b1b
Endogen <Endogen@users.noreply.github.com>
2025-04-14 09:50:56 +0200

    Adjust to new event logic

--- plg/send/send.py
+++ plg/send/send.py
@@ -130,19 +130,19 @@ class Send(TGBFPlugin):
 
         tx_hash = send['tx_hash']
 
-        async def tx_result(success: str, result: str):
-            if not success:
-                await message.edit_text(f"{con.STOP} {result}")
-            else:
-                explorer_url = self.cfg_global.get('xian', 'explorer')
-                link = f'<a href="{explorer_url}/tx/{tx_hash}">View Transaction</a>'
+        if send['success']:
+            async def tx_result(success: str, result: str):
+                if success:
+                    explorer_url = self.cfg_global.get('xian', 'explorer')
+                    link = f'<a href="{explorer_url}/tx/{tx_hash}">View Transaction</a>'
 
-                await message.edit_text(
-                    f"{con.MONEY} Sent <code>{amount}</code> {ticker}\n{link}",
-                    disable_web_page_preview=True
-                )
+                    await message.edit_text(
+                        f"{con.MONEY} Sent <code>{amount}</code> {ticker}\n{link}",
+                        disable_web_page_preview=True
+                    )
+                else:
+                    await message.edit_text(f"{con.STOP} {result}")
 
-        if not send['success']:
-            await message.edit_text(f"{con.STOP} {send['message']}")
-        else:
             await self.plugins['event'].track_tx(tx_hash, tx_result)
+        else:
+            await message.edit_text(f"{con.STOP} {send['message']}")
